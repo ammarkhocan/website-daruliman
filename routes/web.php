e6a2dd87784074b0c679caec68d8851305b9f208
ammae khocan <ammarkhocan02@gmail.com>
2024-10-07 13:07:27 +0700

    membuat layout dan berita

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/berita', function () {
+    return view('berita.berita');
+});
+
+Route::get('/detail', function () {
+    return view('berita.detail');
+});
