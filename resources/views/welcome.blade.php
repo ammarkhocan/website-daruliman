4edf6fcd1db77aab5eb05216b03daa8f6f3a63c8
ammae khocan <ammarkhocan02@gmail.com>
2024-10-07 09:20:29 +0700

    menambahkan animasin dan mengatur responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <link rel="shortcut icon" href="{{ asset('assets/icons/ic-logo-ponpes.ico') }}">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
         <title>Pondok Pesantren Darul Iman</title>
 
@@ -52,7 +53,7 @@
 
         <section id="hero">
             <div class="container text-center text-white">
-                <div class="hero-title">                
+                <div class="hero-title" data-aos="fade-up">                
                     <div class="hero-text">Selamat Datang Di Pondok Pesantren Darul Iman</div>
                     <p>Sekolah terbaik untuk generasi muda yang terampil, berkarakter dan berwawasan Qur'ani</p>
                 </div>
@@ -60,33 +61,33 @@
         </section>
 
         <section id="program" style="margin-top: -30px">
-          <div class="container">
+          <div class="container col-xxl-9">
             <div class="row">
-              <div class="col-lg-3">
-                <div class="bg-white rounded-3 shadow p-3 d-flex justify-content-between align-item-center">
+              <div class="col-lg-3 col-md-6 col mb-2" data-aos="flip-down">
+                <div class="bg-white rounded-3 shadow p-3 mb-2 d-flex justify-content-between align-item-center">
                   <div>
                     <h5>Pendidikan<br>Berkualitas</h5>
                   </div>
                   <img src="{{ asset('assets/icons/ic-book.png') }}" height="55" width="55" alt="">
                 </div>
               </div>
-              <div class="col-lg-3">
-                <div class="bg-white rounded-3 shadow p-3 d-flex justify-content-between align-item-center">
+              <div class="col-lg-3 col-md-6 col mb-2" data-aos="flip-down">
+                <div class="bg-white rounded-3 shadow p-3 mb-2 d-flex justify-content-between align-item-center">
                   <div>
                     <h5>Pendidikan<br>Berkarakter</h5>
                   </div>
                   <img src="{{ asset('assets/icons/ic-globe.png') }}" height="55" width="55" alt="">
                 </div>
               </div>
-              <div class="col-lg-3">
-                <div class="bg-white rounded-3 shadow p-3 d-flex justify-content-between align-item-center">
+              <div class="col-lg-3 col-md-6 col mb-2" data-aos="flip-down">
+                <div class="bg-white rounded-3 shadow p-3 mb-2 d-flex justify-content-between align-item-center">
                   <div>
                     <h5>Pendidikan<br>Muammalah</h5>
                   </div>
                   <img src="{{ asset('assets/icons/ic-neraca.png') }}" height="55" width="55" alt="">
                 </div>
               </div>
-              <div class="col-lg-3">
+              <div class="col-lg-3 col-md-6 col mb-2" data-aos="flip-down">
                 <div class="bg-white rounded-3 shadow p-3 d-flex justify-content-between align-item-center">
                   <div>
                     <h5>Pendidikan<br>Teknologi</h5>
@@ -105,7 +106,7 @@
               <h2 class="fw-bold">Berita Kegiatan Pondok Pesantren</h2>
             </div>
 
-            <div class="row py-5">
+            <div class="row py-5" data-aos="zoom-in">
               <div class="col-lg-4">
                 <div class="card border-0">
                   <img src="{{ asset('assets/images/il-berita-01.png') }}" class="img-fluid mb-3" alt="">
@@ -152,7 +153,7 @@
         <section id="join" class="py-5">
           <div class="container py-5">
             <div class="row d-flex align-items-center">
-              <div class="col-lg-6">
+              <div class="col-lg-6" data-aos="zoom-in-right"> 
                 <div class="d-flex align-item-center mb-3">
                   <div class="stripe me-2"></div>
                   <h5>Daftar Santri</h5>
@@ -162,7 +163,7 @@
                    dengan meluluskan santi dan menjadi ustadz terkemuka mendakwahkan di pelosok nusantara</p>
                    <button class="btn btn-regis">Register</button>
               </div>  
-              <div class="col-lg-6">
+              <div class="col-lg-6" data-aos="zoom-in-left">
                 <img src="{{ asset('assets/images/il-photo-04.png') }}"  class="img-fluid">
               </div>
             </div>
@@ -172,7 +173,7 @@
 
         {{-- Vidio --}}
         <section id="vidio" class="py-5">
-          <div class="container py-5">
+          <div class="container py-5" data-aos="zoom-in">
             <div class="text-center">
               <iframe width="560" height="315" src="https://www.youtube.com/embed/FKIn3wV8rn0?si=0RFkgM00rra910Uo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
@@ -180,19 +181,19 @@
         </section>
         {{-- Vidio --}}
 
-        <section id="vidio_youtube" class="py-5">
+        <section id="vidio_youtube" class="py-5" data-aos="zoom-in">
           <div class="container">
             <div class="header-berita text-center mb-5">
               <h2 class="fw-bold">Vidio kegiatan Pondok Pesantren</h2>
             </div>
             <div class="row py-5">
-              <div class="col-lg-4">
+              <div class="col-lg-4" data-aos="zoom-in-up">
                 <iframe width="100%" height="215" src="https://www.youtube.com/embed/FKIn3wV8rn0?si=CutsiuwqWwu4Dk5N" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
               </div>
-              <div class="col-lg-4">
+              <div class="col-lg-4" data-aos="zoom-in-up">
                 <iframe width="100%" height="215" src="https://www.youtube.com/embed/FKIn3wV8rn0?si=CutsiuwqWwu4Dk5N" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
               </div>
-              <div class="col-lg-4">
+              <div class="col-lg-4" data-aos="zoom-in-up">
                 <iframe width="100%" height="215" src="https://www.youtube.com/embed/FKIn3wV8rn0?si=CutsiuwqWwu4Dk5N" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
               </div>
             </div>
@@ -203,7 +204,7 @@
         </section>
 
         {{--  --}}
-        <section id="foto" class="section-foto parallax">
+        <section id="foto" class="section-foto parallax" data-aos="zoom-in-up">
           <div class="container">
             <div class="d-flex justify-content-between alignt-items-center mb-5">
               <div class="d-flex align-items-center">
@@ -236,7 +237,7 @@
         {{--  --}}
 
         {{-- Fasilitas --}}
-        <section id="fasilitas" class="py-5">
+        <section id="fasilitas" class="py-5" data-aos="zoom-in-up">
           <div class="container py-5">
             <div class="text-center mb-5">
               <div>
@@ -252,7 +253,7 @@
         {{-- Fasilitas --}}
 
         {{-- Footer --}}
-        <section id="footer" class="bg-white">
+        <section id="footer" class="bg-white" data-aos="zoom-out">
           <div class="container py-4" >
             <footer>
               <div class="row">
@@ -311,6 +312,7 @@
 
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
         <script>
           const navbar = document.querySelector('.fixed-top');
           window.onscroll = () => {
@@ -323,7 +325,12 @@
                   navbar.classList.add("navbar-dark");
               }
           }
+
         </script>
+<script>
+  AOS.init();
+</script>
+        
       
     </body>
 </html>
