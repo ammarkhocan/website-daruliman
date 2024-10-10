9e4a2668f316107fce755f104054e331101fad75
ammae khocan <ammarkhocan02@gmail.com>
2024-10-10 11:23:12 +0700

    Memodifikasi halaman admin index, menambahkan BlogController dan folder blog di admin

---
+++ resources/views/admin/blog/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.layouts')
+
+@section('content')
+
+<section style="margin-top: 100px">
+    <div class="container py-5">
+        <h3 class="fw-bold mb-3">Halaman Management Artikel</h3>
+        <p>Atur dan kelola artikel kegiatan pesantren</p>
+
+        <div class="d-flex">
+            <a href="{{ route('dashboard') }}">Home</a>
+            <div class="mx-1">/</div>
+            <a href="{{ route('dashboard') }}">Blog Artikel</a>
+
+        </div>
+    </div>
+</section>
+@endsection
